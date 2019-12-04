dc190e72928fbdb831749dad0b36d79d8ebc6831
771832676 <49219165+771832676@users.noreply.github.com>
2019-12-04 14:11:28 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/mr/wordcount/WordcountReducer.java
@@ -0,0 +1,29 @@
+package com.atguigu.mr.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+//KEYIN,VALUEIN  map阶段输出的key和value
+public class WordcountReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+    IntWritable v = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        //atguigu,1
+        //atguigu,1
+        int sum = 0;
+        //1.累加求和
+        for (IntWritable value: values) {
+            sum += value.get();
+        }
+        v.set(sum);
+        //2.写出 atgueigu2
+        context.write(key,v);
+
+    }
+}
